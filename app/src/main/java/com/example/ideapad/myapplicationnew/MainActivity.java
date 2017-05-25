25100e1f617575b956e3269679f038a3661d8376
yogesh20 <waniyogeshru2015@gmail.com>
2017-05-25 12:55:33 +0530

    TEST

---
+++ app/src/main/java/com/example/ideapad/myapplicationnew/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.ideapad.myapplicationnew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // super code type here
+    }
+}
