fcc3f31379d1f0c93f6b2b475d7d48d17b263c5e
yog2017 <waniyogeshrup@gmail.com>
2017-05-25 19:01:47 +0530

    Update MainActivity.java
    Removed that text

--- app/src/main/java/com/example/ideapad/myapplicationnew/MainActivity.java
+++ app/src/main/java/com/example/ideapad/myapplicationnew/MainActivity.java
@@ -10,9 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int a=500;
-        int b=90;
-
-        int c=a*b;
+     // removed changes 
     }
 }
