8887ff84e7df140a5ffa76749678152c0f9d8ba9
yogesh20 <waniyogeshru2015@gmail.com>
2017-05-25 19:30:10 +0530

    new

--- app/src/main/java/com/example/ideapad/myapplicationnew/MainActivity.java
+++ app/src/main/java/com/example/ideapad/myapplicationnew/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ideapad.myapplicationnew;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
