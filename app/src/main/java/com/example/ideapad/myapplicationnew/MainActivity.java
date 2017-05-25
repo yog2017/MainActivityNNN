222ef30716a0322f321eacf6cea49137f2b61e6c
yog2017 <waniyogeshrup@gmail.com>
2017-05-25 20:05:35 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/ideapad/myapplicationnew/MainActivity.java
+++ app/src/main/java/com/example/ideapad/myapplicationnew/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-       // Test branch - some code 
+       // Test branch - Theire code 
 
 
     }
