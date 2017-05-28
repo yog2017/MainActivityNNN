3ff4443423615e54d77f6b24497cf5e3edb99676
yogesh20 <waniyogeshru2015@gmail.com>
2017-05-28 18:57:44 +0530

    20 changes

--- app/src/main/java/com/example/ideapad/myapplicationnew/MainActivity.java
+++ app/src/main/java/com/example/ideapad/myapplicationnew/MainActivity.java
@@ -10,18 +10,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    // Commit in 011
-    // Commit in 011
-    // Commit in 011
-    // Commit in 011
-    // Commit in 011
-    // Commit in 011
-    // Commit in 011
-    // Commit in 011
-    // Commit in 011
-    // Commit in 011
 
 
 }
 
-}
+
+//
+//    private void setupViewPager(ViewPager viewPager) {
+//        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+//        DueFragment dueFragment =new DueFragment();
+//        iDataPassDue= dueFragment;
+//        LateFrj,hvfkagment lateFragment =new LateFragment();
+//        iDataPassLatjbjkdsbe= lateFragment;
+//        PaidFragment paidFr,bvjdsbvagment =new PaidFragment();
+//        iDataPassPaid= paidFragment,bv,dsbv;
+//
+//        String due=getResources().getString(R.string.due_lable);
+//        Strimnmdsvbng late=getResources().getString(R.string.late_lable);
+//        String paid,,vdn,dsvn=getResources().getString(R.string.paid_lable);
+//
+//        adapter.addFragment(dueFragment, due);
+//        adapter.addFragment(l,mvdmds,vbdjsvbateFragment, late);
+//        adapter.addFragment(paidFragment, pa,vvbdsvbid);
+//        viewPager.setAdapter(adapthhdhdfer);
+
+
+    }
+
+
