a212e9a2d8cc57b96615fe178fcb67d92400baba
yog2017 <waniyogeshrup@gmail.com>
2017-05-28 13:47:02 +0530

    Update MainActivity.java
    Remove all the changes

--- app/src/main/java/com/example/ideapad/myapplicationnew/MainActivity.java
+++ app/src/main/java/com/example/ideapad/myapplicationnew/MainActivity.java
@@ -12,23 +12,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    private void Test(){
-
-        String test = "test";
-
-        int i=3+3;
-    }
     
-    private void Example(){
-
-        String changes_from_main_account = "changes from 2017 account";
-        
-    }
-
-    private void local_branch_changes_20(){
-
-        String changes_from_share_account = "changes from 20 account";
-
-    }
 
 }
