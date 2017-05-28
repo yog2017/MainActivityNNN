84d04cc637924d5e1bea0486d8b8c4481003bab5
yog2017 <waniyogeshrup@gmail.com>
2017-05-28 16:49:50 +0530

    Update MainActivity.java
    fbewhkfbewhfbefhbef

--- app/src/main/java/com/example/ideapad/myapplicationnew/MainActivity.java
+++ app/src/main/java/com/example/ideapad/myapplicationnew/MainActivity.java
@@ -10,9 +10,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);}
 
-    void multiplication(){
 
-        //we added multiplication
+    void modificationFile(){
+    
+    //jhdhjwbfhwbfjwfbwjfbwjjfbwjfbwfbewfbewfjb
     }
 
     }
