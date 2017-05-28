e0f35ded8dcfb6cb626a6c0fa282dc5f6b868477
yog2017 <waniyogeshrup@gmail.com>
2017-05-28 15:38:50 +0530

    Update MainActivity.java
    some updtaes

--- app/src/main/java/com/example/ideapad/myapplicationnew/MainActivity.java
+++ app/src/main/java/com/example/ideapad/myapplicationnew/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        
+        // updtaes from 2017
 
     }
     
